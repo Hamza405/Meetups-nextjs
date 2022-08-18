a4d09257a60b9af73de72d58bfe8736d1d21c3dc
Hamza <hamzash863@gmail.com>
2022-08-18 03:59:21 -0400

    Add meetups server side function

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,21 @@
-function handler ( req, res ) {
+import { MongoClient } from "mongodb";
+
+async function handler ( req, res ) {
     if ( req.method === 'POST' )
     {
         const data = req.body;
         const { title, image, address, description } = data;
 
+        const client = await MongoClient.connect( 'localhost:27017/meetups' );
+        const db = client.db();
+
+        const meetupsColl = db.collection( 'meetups' );
+        const result = meetupsColl.insertOne( data );
+
+        console.log( result );
+        client.close();
+
+        res.status( 201 ).json( { message: 'added succcess' } );
     }
 }
 
